c537312f68989ffb3ecc5bb505b1c06af8c3dee7
Walles069 <lesikvalentin2000@gmail.com>
2021-02-28 18:48:36 +0200

    initial commit

---
+++ Console_EStudy/Console_EStudy/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Console_EStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Developed by: IA-71 students - Gritsachenko A.S. Lesyk V.O.");
+            Console.WriteLine("VERSION 1");
+            Console.WriteLine("Modelling start");
+            Console.ReadKey();
+            Console.ReadKey();
+            Console.ReadKey();
+        }
+    }
+}
